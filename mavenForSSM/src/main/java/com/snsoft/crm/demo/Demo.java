089d3629b249b8830b45cb799dc5466f02c7e6c9
DESKTOP-UETDJ0T\blairscott <limingming@snsoft.com.cn>
2018-02-14 21:36:31 +0800

    201802142135:spring中使用注解代替xml文件

--- mavenForSSM/src/main/java/com/snsoft/crm/demo/Demo.java
+++ mavenForSSM/src/main/java/com/snsoft/crm/demo/Demo.java
@@ -5,6 +5,7 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import com.snsoft.crm.bean.CollectionBean;
+import com.snsoft.crm.bean.Employee;
 import com.snsoft.crm.bean.User;
 
 public class Demo {
@@ -17,7 +18,7 @@ public class Demo {
 		//3.打印user对象
 		System.out.println(user);
 	}
-	
+
 	@Test
 	public void test02() {
 		//1.创建容器对象
@@ -26,5 +27,18 @@ public class Demo {
 		CollectionBean cb = (CollectionBean) ac.getBean("cb");
 		//3.打印user对象
 		System.out.println(cb);
+	}
+
+	@Test
+	public void test03() {
+		//1.创建容器对象
+		ClassPathXmlApplicationContext ac = new ClassPathXmlApplicationContext("applicationContext02/applicationContext.xml");
+		//2.向容器"要"对象
+		Employee ep1 = (Employee) ac.getBean("employee");
+		Employee ep2 = (Employee) ac.getBean("employee");
+		System.out.println(ep1 == ep2);
+		//3.打印user对象
+		System.out.println(ep1);
+		ac.close();
 	}
 }
